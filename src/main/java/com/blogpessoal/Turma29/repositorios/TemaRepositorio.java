f3894b9ed534ad5303896807904f2d2c0e4d1251
charlô <charlesoliveira0293@hotmail.com>
2021-08-26 01:11:01 -0300

    Criação do CRUD para Tema

--- src/main/java/com/blogpessoal/Turma29/repositorios/TemaRepositorio.java
+++ src/main/java/com/blogpessoal/Turma29/repositorios/TemaRepositorio.java
@@ -1,11 +1,20 @@
 package com.blogpessoal.Turma29.repositorios;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.blogpessoal.Turma29.modelos.Tema;
-
 @Repository
 public interface TemaRepositorio extends JpaRepository<Tema, Long> {
 
+	/**
+	 * Método utilizado para pesquisar coluna tema ContainigIgnoreCase
+	 * 
+	 * @param nome do tipo String
+	 * @return List de Temas
+	 * @author charlô
+	 */
+	List<Tema> findAllByTemaContainingIgnoreCase(String tema);
 }
