b9e7f5c747c7d34c900eea4bc172da539b242e3b
charlô <charlesoliveira0293@hotmail.com>
2021-08-26 00:57:44 -0300

    Criação do CRUD para Postagem

--- src/main/java/com/blogpessoal/Turma29/repositorios/PostagemRepositorio.java
+++ src/main/java/com/blogpessoal/Turma29/repositorios/PostagemRepositorio.java
@@ -1,5 +1,7 @@
 package com.blogpessoal.Turma29.repositorios;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,14 @@ import com.blogpessoal.Turma29.modelos.Postagem;
 
 @Repository
 public interface PostagemRepositorio extends JpaRepository<Postagem, Long> {
+	
+	/**
+	 * Método utilizado para pesquisar coluna titulo ContainigIgnoreCase
+	 * 
+	 * @param titulo do tipo String ContainingIgnoreCase
+	 * @return List de Postagens
+	 * @author charlô
+	 */
+	List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 
 }
